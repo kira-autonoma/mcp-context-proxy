f3eda1354fdfeab8ee954ec02a797c2bbb0bc3da
Kira Autonoma <kiraautonoma@gmail.com>
2026-03-17 12:41:48 +0000

    chore: add .gitignore, remove node_modules and dist from tracking

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { MCPContextProxy } from "./proxy-server";
-export { SchemaStore } from "./schema-cache";
-export { computeSavings, estimateTokens, schemaTokens } from "./token-estimator";
-export * from "./types";
-//# sourceMappingURL=index.d.ts.map
